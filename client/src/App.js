8852f1b63a1adef14363b11167d401d003b95f99
Festim1234hub <fismaili792@gmail.com>
2025-05-29 13:09:46 +0200

    Forgot to push

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { MovieProvider } from './context/MovieContext';
+
+// Components
+import Navbar from '../components/Layout/Navbar';
+import Footer from '../components/Layout/Footer';
+
+// Pages
+import HomePage from './pages/HomePage';
+import MoviesPage from './pages/MoviesPage';
+import MovieDetailPage from './pages/MovieDetailPage';
+
+
+// Admin Pages
+import AdminMovies from './pages/Admin/AdminMovies';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MovieProvider>
+      <Router>
+        <div className="App min-h-screen bg-dark-900 text-gray-100">
+          <Navbar />
+          <main className="min-h-screen">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/movies" element={<MoviesPage />} />
+              <Route path="/movies/:id" element={<MovieDetailPage />} />
+              <Route path="/admin/movies" element={<AdminMovies />} />
+            </Routes>
+          </main>
+          <Footer />
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
+        </div>
+      </Router>
+    </MovieProvider>
   );
 }
 
